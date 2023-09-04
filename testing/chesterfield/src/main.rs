0d0abdaaf79f51b2e2921a691ef34d02877d5bed
ajaykumargdr <ajaykumarncas@gmail.com>
2023-09-04 19:02:18 +0530

    chore: add postgres db and couch db crud operations practice code

--- testing/chesterfield/src/main.rs
+++ testing/chesterfield/src/main.rs
@@ -1,13 +1,18 @@
+/* 
 use reqwest;
-use serde_json::json;
+// use serde_json::json;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
     let client = reqwest::Client::new();
     let db_name = "my_database";
 
+    // Client::connect("postgresql://postgres:pgdb9488@localhost:5432/akdb", NoTls)
+
     // Create a new database
-    let create_db_url = format!("http://localhost:5984/{}", db_name);
+    let create_db_url = format!("http://ajaykumarm:db9488@localhost:5984/{}", db_name);
+
     let response = client
         .put(&create_db_url)
         .send()
@@ -20,5 +25,99 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     Ok(())
+} */
+
+use chesterfield::sync::Client;
+use chesterfield::GetResponse;
+
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct TestData{
+    name: String,
+    data: i32,
 }
 
+// db getting connection
+pub fn get_connection_client() -> Client{
+
+    Client::new("http://ajaykumarm:db9488@localhost:5984/")
+            .expect("cannot connect to the data base")
+
+}
+
+pub fn create_database_if_not_exist(client: &mut Client, db_name: &str){
+
+   let db =  client.database(db_name).expect("cannot find the data base");
+
+   if !db.exists().unwrap(){
+        db.create().expect("not able to create database");
+   }
+
+   println!("database successfully created");
+}  
+
+pub fn create_document(client: &mut Client, db_name:&str, data: TestData) -> String{
+
+    let db = client.database(db_name).expect("cannot connect to the given data base");
+
+    db
+        .insert(&data, None)
+        .send()
+        .expect("not able to create document")
+        .id
+}
+
+pub fn get_document(client: &mut Client, db_name: &str, doc_id: &str) -> TestData{
+
+    let db = client.database(db_name).expect("cannot connect to the given data base");
+
+    println!("---- 1 ------");
+
+    // this method call prints some unnecessary data
+
+    let doc_get_response:GetResponse<TestData> = db
+        .get(doc_id)
+        .send()
+        .expect("cannot retrieve document from the database");
+
+        
+    println!("---- 2 ------");
+
+    let document:TestData = doc_get_response
+        .into_inner()
+        .expect("not able to get the document from the response");
+
+        
+    println!("---- 3 ------");
+    
+    document
+
+
+}
+
+fn main(){
+
+    let mut client = get_connection_client();
+
+    let db_name = String::from("test_db");
+
+    // Create db
+    create_database_if_not_exist(&mut client, &db_name);
+    
+    // create document
+    
+    let data = TestData{
+        name: String::from("Rustian"),
+        data: 1
+    };
+
+    let doc_id = create_document(&mut client, &db_name, data);
+
+    // get document
+
+    let doc = get_document(&mut client, &db_name, &doc_id );
+
+    println!("{:?}", doc);
+
+}
---
+++ testing/postgres_crud_operations/src/main.rs
@@ -0,0 +1,200 @@
+use postgres::{Client, Error, NoTls};
+use std::{io, process::exit};
+
+pub fn controller(menu_choice: String, client: &mut Client)-> Result<(), Error>{
+
+    println!("{menu_choice}");
+
+    match menu_choice.trim().parse::<i32>().unwrap(){
+        1 => {
+            insert(client)?;
+            println!("\nRequest successfully inserted");
+        }
+        2 => {
+            edit(client)?;
+            println!("\nRow successfully update");
+        }
+        3 => {
+            delete(client)?;
+            println!("\nRow successfully deleted");
+        }
+        4 => {
+            display(client)?;
+        }
+        5 => {
+            query(client)?;
+        }
+        6 => {
+            exit(0)
+        }
+        _ => println!("Option is wrong!"),
+    }
+
+    Ok(())
+}
+
+pub fn get_connection_client() -> Result<Client, Error> {
+    Client::connect("postgresql://postgres:pgdb9488@localhost:5432/akdb", NoTls)
+}
+
+pub fn create_table_if_not_exist(client: &mut Client) -> Result<(), Error> {
+    client.execute("CREATE TABLE IF NOT EXISTS person ( id SERIAL PRIMARY KEY, name VARCHAR(30), data VARCHAR(150));", &[])?;
+    Ok(())
+}
+
+pub fn insert(client: &mut Client) -> Result<(), Error> {
+    println!("");
+    println!("*************");
+    println!("Input Data");
+    println!("*************");
+    let mut username = String::new();
+    println!("Enter the person name below :");
+
+    io::stdin()
+        .read_line(&mut username)
+        .expect("failed to readline");
+
+    let mut data = String::new();
+
+    io::stdin()
+        .read_line(&mut data)
+        .expect("failed to readline");
+
+    /////////////////////////////
+    println!("|{data}|");
+
+    client.execute(
+        "INSERT INTO person (name, data) VALUES ($1, $2)",
+        &[&username, &data],
+    )?;
+
+    Ok(())
+}
+
+pub fn edit(client: &mut Client) -> Result<(), Error> {
+
+    println!("\n***********Edit Person Details***********\n");
+
+    display(client)?;
+
+    // Getting the Person Id
+    println!("Enter person Id:");
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("failed to readline");
+    println!("");
+
+    // getting new data
+    println!("Enter person details:");
+
+    let mut new_username = String::new();
+
+    io::stdin()
+        .read_line(&mut new_username)
+        .expect("error input");
+
+    let mut new_user_data = String::new();
+
+    io::stdin()
+        .read_line(&mut new_user_data)
+        .expect("error input");
+
+    let index = index.trim().parse::<i32>().unwrap();
+
+    client.execute(
+        "UPDATE person SET name=$1,data=$2 WHERE id=$3;",
+        &[&new_username, &new_user_data, &index],
+    )?;
+
+    Ok(())
+}
+
+pub fn display(client: &mut Client) -> Result<(), Error> {
+
+    println!("*****************\nDisplay All Data!\n*****************");
+
+    for row in client.query("SELECT id, name, data FROM person", &[])? {
+        let id: i32 = row.get(0);
+        let name: &str = row.get(1);
+        let data: &str = row.get(2);
+
+        println!("Id: {} |Name: {} |Data: {}", id, name, data);
+        println!("-----------------------");
+    }
+
+    Ok(())
+}
+
+pub fn delete(client: &mut Client) -> Result<(), Error> {
+
+    println!("\n***********\nDelete Data\n***********");
+
+    display(client)?;
+    println!("Enter person id: ");
+
+    let mut index = String::new();
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Please enter a number");
+    let index = index.trim().parse::<i32>().unwrap();
+    
+    client.execute("DELETE FROM person WHERE id = $1;",&[&index])?;
+
+    Ok(())
+}
+
+pub fn query(client: &mut Client) -> Result<(), Error> {
+    println!("");
+    println!("***********");
+    println!("Query a row");
+    println!("***********");
+    println!("");
+    println!("Enter an id:");
+    let mut index = String::new();
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Please enter a number");
+    let index = index.trim().parse::<i32>().unwrap();
+    for row in client.query("SELECT  name, data FROM person  WHERE id =$1", &[&index])? {
+        let name: &str = row.get(0);
+        // let data: Option<&[u8]> = row.get(1);
+
+        println!("Selected row has {}  ", name);
+    }
+
+    Ok(())
+}
+
+fn main() -> Result<(), Error> {
+
+    let mut client: Client = get_connection_client()?;
+
+    // deleting table (developing purpose)
+    // client.execute("DROP TABLE person;", &[])?;
+
+    create_table_if_not_exist(&mut client)?;
+
+    loop {
+
+        // Menu
+        println!("\n
+            1. Insert Data\n
+            2. Edit Data\n
+            3. Delete Data\n
+            4. Display Data\n
+            5. Query Data\n
+            6. Exit");
+
+        let mut menu_choice = String::new();
+
+        io::stdin()
+            .read_line(&mut menu_choice)
+            .expect("failed to read the input");
+
+        // Matching the option
+        controller(menu_choice, &mut client)?;
+    }
+
+}
